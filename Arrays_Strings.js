ce8367b894cf0a7e3433bc04234c92e6eeb4e3cb
Stephen Kang <kang.stephen94@gmail.com>
2018-08-15 01:25:42 -0700

    Second Problem

--- Arrays_Strings.js
+++ Arrays_Strings.js
@@ -4,8 +4,7 @@ function urlify (string, n) {
   //Iterate through all characters in the string and check to see if its a whitespace
   let result = '';
   for (var i = 0; i < string.length; i++ ) {
-    if ( string[i] = ' ' && i < n ) {
-      
+    if ( string[i] === ' ' && i < n ) {
       result = result + '%20'
     } else {
       result = result + string[i]
@@ -13,7 +12,13 @@ function urlify (string, n) {
   }
 
   console.log(result);
-  return result;
 }
 
+//Test
 urlify('Mr John Smith     ', 13)
+
+// Palindrome Permutation
+
+function palindrome_perm (string) {
+  
+}

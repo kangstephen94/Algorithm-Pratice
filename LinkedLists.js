76fb1198fa481ab73b244d3f45cd3eb008474675
Stephen Kang <kang.stephen94@gmail.com>
2018-08-16 16:44:49 -0700

    Sum of linked lists

--- LinkedLists.js
+++ LinkedLists.js
@@ -28,3 +28,60 @@ function kthElement (linkedList, k) {
 
   return currentNode2
 }
+
+function deleteMiddle (node) {
+  node.prev.next = node.next
+  node.next.prev = node.prev
+}
+
+//Sum Lists
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+//  */
+var addTwoNumbers = function (l1, l2) {
+
+  var result = new ListNode();
+  var current = result;
+  var carry = 0;
+
+  while (l1 !== null || l2 !== null) {
+
+    if (current === null) {
+      current = new ListNode();
+      result.next = current;
+    }
+
+    const v1 = l1.val || 0;
+    const v2 = l2.val || 0;
+
+    const sum = v1 + v2;
+    carry = Math.floor(sum / 10);
+    value = current.val ? sum % 10 + current.val : sum % 10
+
+    current.val = value;
+
+    console.log(current);
+
+    l1 = l1.next
+    l2 = l2.next
+
+    if (carry === 1) {
+      current.next = new ListNode(1)
+    }
+
+    current = current.next
+
+
+  }
+
+  return result;
+}

dcbb53c1ce9d5ae812185beb591dda1788aad7ad
Stephen Kang <kang.stephen94@gmail.com>
2018-08-15 21:18:03 -0700

    Finish problem 2.1

--- LinkedLists.js
+++ LinkedLists.js
@@ -1,5 +1,11 @@
 // Write code to remove duplicates from an unsorted linked list
 
 function removeDup (linkedList) {
-  linkedList
+  const hashMap = new hashMap();
+  linkedList.each ( node => {
+    if (hashMap[node.value]) {
+      node.delete()
+    }
+    hashMap[node.value] = true
+  })
 }

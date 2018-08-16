03a9dcbc60af03809c52cc6e55a859437a1cf246
Stephen Kang <kang.stephen94@gmail.com>
2018-08-15 21:24:52 -0700

    Implement Kth element in linkedlist

--- LinkedLists.js
+++ LinkedLists.js
@@ -1,4 +1,5 @@
 // Write code to remove duplicates from an unsorted linked list
+// Assume hashMap, node and linkedList classes are already defined.
 
 function removeDup (linkedList) {
   const hashMap = new hashMap();
@@ -8,4 +9,22 @@ function removeDup (linkedList) {
     }
     hashMap[node.value] = true
   })
+}
+
+// Implement an algorithm to find the kth to last element of a single linked list
+
+function kthElement (linkedList, k) {
+  var currentNode1 = linkedList.head
+  var currentNode2 = linkedList.head
+
+  for (var i = 0; i < k; i++) {
+    currentNode1 = currentNode1.next
+  }
+
+  while (currentNode1 !== null) {
+    currentNode1 = currentNode1.next 
+    currentNode2 = currentNode2.next
+  }
+
+  return currentNode2
 }

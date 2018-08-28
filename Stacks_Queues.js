6c81a9b6e12a6fd4b44e4f60925783581d383982
Stephen Kang <kang.stephen94@gmail.com>
2018-08-27 23:51:23 -0700

    Window max range

--- Stacks_Queues.js
+++ Stacks_Queues.js
@@ -21,7 +21,7 @@ class Stack {
 
   pop () {
     const last = this.values.pop();
-    return last[0];
+    return last;
   }
 
   min() {
@@ -29,9 +29,12 @@ class Stack {
   }
 
   max() {
-    return this.values[this.values.length-1][2]
+    return this.values[this.values.length - 1][2]
   }
 
+  length () {
+    return this.values.length
+  }
 }
 
 //testing
@@ -42,7 +45,7 @@ class Stack {
 // stack.push(9);
 // stack.push(1);
 // stack.max();
-// Stack.min();
+// stack.min();
 
 //Stack of Plates
 
@@ -57,49 +60,114 @@ class Stack {
 
 class StackQueue {
   constructor () {
-    this.in = Stack.new ();
-    this.out = Stack.new ();
+    this.in = new Stack();
+    this.out = new Stack();
   }
 
+  // O(1)
   enqueue (element) {
-   return this.in.push(element)
+    return this.in.push(element)
   }
 
+  // O(N) Linear time, but amortized is O(1) constant.
   dequeue () {
-    if (this.out.length === 0) {
-      while (this.in.length > 0) {
-        this.out.push(this.pop());
+    if (this.out.length() === 0) {
+      while (this.in.length() > 0) {
+        this.out.push(this.in.pop()[0]);
       }
     }
     return this.out.pop();
   }
+
+  min () {
+    if (this.out.length() === 0) {
+      console.log('out is empty')
+      return this.in.min();
+    } else {
+      console.log(this.out.min())
+      return Math.min(...[this.in.min(), this.out.min()])
+    }
+  }
+  
+  max () {
+    if (this.out.length () === 0) {
+      console.log('out is empty')
+      return this.in.max();
+    } else {
+      console.log(this.out.max())
+      return Math.max(...[this.in.max(), this.out.max()])
+    }
+  }
+
+  length () {
+    return(this.in.length() + this.out.length());
+  }
+
 }
 
 //testing
 
-const stackq = new StackQueue();
-stackq.enqueue(2);
-stackq.enqueue(5);
-stackq.enqueue(6);
-stackq.enqueue(7);
+// const stackq = new StackQueue();
+// stackq.enqueue(2);
+// stackq.enqueue(5);
+// stackq.enqueue(6);
+// stackq.enqueue(7);
 
-stackq.dequeue();
-stackq.dequeue();
+// stackq.dequeue();
+// stackq.dequeue();
 
 
 // Sort Stack, smallest elements on the top
 
-function sortStack (stackA) {
-  var stackB = new Stack();
-  while (!stackA.isEmpty()) {
-    const int = stackA.pop();
-    while (!stackB.isEmpty() && stackB.peek() > int){
-      stackA.push(stackB.pop());
-    }
-    stackB.push(int);
-  }
+// function sortStack (stackA) {
+//   var stackB = new Stack();
+//   while (!stackA.isEmpty()) {
+//     const int = stackA.pop();
+//     while (!stackB.isEmpty() && stackB.peek() > int){
+//       stackA.push(stackB.pop());
+//     }
+//     stackB.push(int);
+//   }
 
-  while (!stackB.isEmpty()) {
-    stackA.push(stackB.pop())
+//   while (!stackB.isEmpty()) {
+//     stackA.push(stackB.pop())
+//   }
+
+// }
+
+
+
+
+// Given an array, and a window size w, find the maximum max - min within a range of w elements.
+
+
+// This problem sounds like we need to implement a min/max stack, with a queue of size w.  
+// We can constantly push elements in and out and track the min and max of those elements.
+
+
+
+function findMaxRange (arr, w) {
+  let maxRange;
+  const StackQueue1 = new StackQueue();
+
+  for (var i = 0; i < arr.length; i++) {
+
+    StackQueue1.enqueue(arr[i])
+    const currentRange = StackQueue1.max() - StackQueue1.min();
+    console.log(currentRange)
+    console.log(StackQueue1)
+
+    if (maxRange === undefined || currentRange > maxRange) {
+        maxRange = currentRange;
+      }
+
+    if (StackQueue1.length() === w) {
+      StackQueue1.dequeue();
+    }
   }
+  return maxRange;
 }
+
+//Testing
+
+console.log(findMaxRange([1,2,25,4,5,6, 120], 3));
